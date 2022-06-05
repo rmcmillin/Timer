a3cb8645b98a37a9727314e16c2d028573a2a83c
rmcmillin <77390891+rmcmillin@users.noreply.github.com>
2022-06-04 23:30:16 -0400

    doxygen

--- timer.c
+++ timer.c
@@ -16,6 +16,13 @@ struct Timer{
 
 static struct Timer timerList [MAX_TIMERS];
 
+
+/** \brief Interrupt Handler
+ *
+ * \details To be run in 1 periodic 1ms interrupt.  Checks for expired timers of type TIMER_INTERRUPT and runs an expiry function if there is one.
+ *
+ */
+
 void interruptHandler(){
     systick++;
     //comment below if you dont want to process timers in interrupt
@@ -37,13 +44,27 @@ void interruptHandler(){
     }
 }
 
-timerRetVal timer_init(){
+/** \brief Timer hardware initialization
+ *
+ * \details MCU dependant code for starting a 1ms timer with interrupt
+ *
+ * \return timerRetVal_t (optional)
+ *
+ */
+
+
+timerRetVal_t timer_init(){
+    timerRetVal_t ret = 0;
     //setup hardware for a 1ms timer interrupt
 
-    return 0;
+    return ret;
 }
 
-//run in main loop to poll for expired timers
+
+/** \brief Process Timers - Run in main loop
+ * \details Checks for expired timers and runs a function on expiry if one is set. Timers with type TIMER_POLL.
+ */
+
 void timer_run(){
     static uint32_t oldTime;
     uint32_t timeNow = timer_getTick();
@@ -68,7 +89,18 @@ void timer_run(){
     }
 }
 
-timerRetVal timer_registerCB(uint8_t *timerID, uint32_t period, timerType_t timerProcessType, action expiry){
+/** \brief Register a timer and set a function to run on timer expiry
+ *
+ * \param timerID   ID of registered timer is set - check for NULL before using
+ * \param period    Time until expired
+ * \param timerProcessTYpe  Select TIMER_POLL or TIMER_INTERRUPT.
+ * \param expiry    Function pointer to function that runs on expiry.
+ * \return          timerRetVal_t
+ *
+ */
+
+
+timerRetVal_t timer_registerCB(uint8_t *timerID, uint32_t period, timerType_t timerProcessType, action expiry){
     int8_t result;
     result = timer_register(timerID, period);
     if (result == TIMER_SUCCESS){
@@ -79,7 +111,15 @@ timerRetVal timer_registerCB(uint8_t *timerID, uint32_t period, timerType_t time
     return result;
 }
 
-timerRetVal timer_register(uint8_t *timerID, uint32_t period){
+/** \brief Register a timer that does not have a function to run on expiry.  Defaults to TIMER_POLL
+ *
+ * \param timerID   ID of timer
+ * \param period    time until timer expires
+ * \return          timerRetVal_t
+ *
+ */
+
+timerRetVal_t timer_register(uint8_t *timerID, uint32_t period){
     for (uint8_t i=0; i<MAX_TIMERS; i++){
         struct Timer *timerInfo = &timerList[i];
         if (timerInfo->status == TIMER_UNUSED){
@@ -94,7 +134,14 @@ timerRetVal timer_register(uint8_t *timerID, uint32_t period){
     return TIMER_ERR_NO_FREE_ID;
 }
 
-timerRetVal timer_free(uint8_t timerID){
+/** \brief Free a Timer ID
+ *  \details    Set a timer to unused so that the ID can be reused elsewhere
+ * \param   timerID ID of timer to free
+ * \return timerRetVal_t
+ *
+ */
+
+timerRetVal_t timer_free(uint8_t timerID){
     if (timerID >= 0 && timerID < MAX_TIMERS){
         struct Timer *timerInfo = &timerList[timerID];
         if (timerInfo->status == TIMER_UNUSED){
@@ -108,7 +155,15 @@ timerRetVal timer_free(uint8_t timerID){
     return TIMER_ERR_ID_OUT_OF_RANGE;
 }
 
-timerRetVal timer_start(uint8_t timerID){
+/** \brief Start Timer
+ *
+ * \param timerID   ID of timer to start
+ * \return timerRetVal_t
+ *
+ */
+
+
+timerRetVal_t timer_start(uint8_t timerID){
     if (timerID >= 0 && timerID < MAX_TIMERS){
         struct Timer *timerInfo = &timerList[timerID];
         timerInfo->startTime = timer_getTick();
@@ -123,7 +178,15 @@ timerRetVal timer_start(uint8_t timerID){
     return TIMER_ERR_ID_OUT_OF_RANGE;
 }
 
-timerRetVal timer_setPeriod(uint8_t timerID, uint32_t period){
+/** \brief Set the period of the timer
+ *
+ * \param timerID   ID of timer to set the period of
+ * \param period    period in ms
+ * \return timerRetVal_t
+ *
+ */
+
+timerRetVal_t timer_setPeriod(uint8_t timerID, uint32_t period){
     if (timerID > 0 && timerID < MAX_TIMERS){
         struct Timer *timerInfo = &timerList[timerID];
         if (timerInfo->status == TIMER_UNUSED){
@@ -133,6 +196,14 @@ timerRetVal timer_setPeriod(uint8_t timerID, uint32_t period){
     }
     return TIMER_ERR_ID_OUT_OF_RANGE;
 }
+
+/** \brief Timer Expiry Checl
+ *
+ * \param timerID   timer to check
+ * \return 1: expired, otherwise 0
+ *
+ */
+
 
 int8_t timer_isExpired(uint8_t timerID){
     if (timerID >= 0 && timerID < MAX_TIMERS){
@@ -147,6 +218,13 @@ int8_t timer_isExpired(uint8_t timerID){
     return TIMER_ERR_ID_OUT_OF_RANGE;
 }
 
+/** \brief Check if timer is running
+ *
+ * \param timerID   ID of timer to check
+ * \return returns 1 if RUNNING otherwise 0
+ *
+ */
+
 int8_t timer_isRunning(uint8_t timerID){
     if (timerID >= 0 && timerID < MAX_TIMERS){
         struct Timer *timerInfo = &timerList[timerID];
@@ -160,6 +238,14 @@ int8_t timer_isRunning(uint8_t timerID){
     return TIMER_ERR_ID_OUT_OF_RANGE;
 }
 
+/** \brief Check time remaining
+ *
+ * \param timerID   ID of timer to check remaining time
+ * \return ms until expiry
+ *
+ */
+
+
 uint32_t timer_timeRemaining (uint8_t timerID){
     if (timerID >= 0 && timerID < MAX_TIMERS){
         struct Timer *timerInfo = &timerList[timerID];
@@ -167,6 +253,13 @@ uint32_t timer_timeRemaining (uint8_t timerID){
     }
     return 0; //error
 }
+
+/** \brief get system tick count
+ *
+ * \return current tick count
+ *
+ */
+
 
 uint32_t timer_getTick(){
     return systick;
--- timer.h
+++ timer.h
@@ -1,12 +1,12 @@
 
 #define MAX_TIMERS  5
 
-typedef enum TimerRetVal{
+typedef enum timerRetVal_t{
     TIMER_ERR_NO_FREE_ID = -3,
     TIMER_ERR_ID_OUT_OF_RANGE,
     TIMER_ERR_ID_UNREGISTERD,
     TIMER_SUCCESS, //0
-}timerRetVal;
+}timerRetVal_t;
 
 typedef enum TimerType{
     TIMER_POLL,
@@ -27,16 +27,16 @@ enum expiryAction {
 
 typedef enum expiryAction (*action)(uint8_t timerID);
 
-timerRetVal timer_init();
+timerRetVal_t timer_init();
 void interruptHandler();
 
 void timer_run();
-timerRetVal timer_registerCB(uint8_t *timerID, uint32_t period, timerType_t timerProcessType, action expiry);
-timerRetVal timer_register(uint8_t *timerID, uint32_t period);
-timerRetVal timer_free(uint8_t timerID);
-timerRetVal timer_start(uint8_t timerID);
-timerRetVal timer_setPeriod(uint8_t timerID, uint32_t period);
-timerRetVal timer_free(uint8_t timerID);
+timerRetVal_t timer_registerCB(uint8_t *timerID, uint32_t period, timerType_t timerProcessType, action expiry);
+timerRetVal_t timer_register(uint8_t *timerID, uint32_t period);
+timerRetVal_t timer_free(uint8_t timerID);
+timerRetVal_t timer_start(uint8_t timerID);
+timerRetVal_t timer_setPeriod(uint8_t timerID, uint32_t period);
+timerRetVal_t timer_free(uint8_t timerID);
 int8_t timer_isExpired(uint8_t timerID);
 int8_t timer_isRunning(uint8_t timerID);
 uint32_t timer_timeRemaining (uint8_t timerID);
